1aa925d961a8f6c64cf0cb5855469990683cc96c
jinheelee04 <59869557+jinheelee04@users.noreply.github.com>
2024-08-06 13:41:48 +0900

    기본 객체들

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,10 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,8 +47,31 @@ public class BasicController {
         model.addAttribute("userMap", map);
         return "basic/variable";
     }
-
-
+    // 스프링부트 3.0 미만
+    @GetMapping("/basic-objects/old")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects-old";
+    }
+    
+    // 스프링부트 3.0 이상
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+    
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+    
     @Data
     static class User {
         private String username;
